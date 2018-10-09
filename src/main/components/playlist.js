abd454359df176e437da2226cbe84704ca418128
Jonzy3000 <jonzy_3000@hotmail.co.uk>
2018-10-10 00:08:30 +0100

    check for empty playlist

--- src/main/components/playlist.js
+++ src/main/components/playlist.js
@@ -5,10 +5,17 @@ const Playlist = props =>
     <div>
         <h1> Hello World {props.name}</h1>
         <br />
-        {props.tracks.map((track) => {
-            return <h6 key={track.track.id}>{track.track.name}</h6>
-        })}
+        {props.track == null || props.track.length == null ?
+            <EmptyPlaylist />
+            :
+            props.tracks.map((track) => {
+                return <h6 key={track.track.id}>{track.track.name}</h6>
+            })
+        }
     </div>
+
+const EmptyPlaylist = () =>
+    <h1>Looks like this playlist is empty, lets add some songs!</h1>
 
 Playlist.propTypes = {
     name: PropTypes.string,
